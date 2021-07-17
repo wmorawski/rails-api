f7733ece262520130715a23b0f9867b103a932f0
Wiktor Morawski <wiktor.morawski@recruitee.com>
2021-07-17 22:26:38 +0200

    test: fix articles_controller_spec.rb

--- spec/requests/articles_controller_spec.rb
+++ spec/requests/articles_controller_spec.rb
@@ -33,18 +33,18 @@ RSpec.describe ArticlesController do
     end
 
     it "paginate results" do
-      _, article2, = create_list(:article, 3)
+      _, article, = create_list(:article, 3)
       get "/articles", params: { page: {number: 2, size: 1} }
       expect(json_data.length).to eq(1)
-      expect(json_data.first[:id]).to eq(article2.id)
+      expect(json_data.first[:id]).to eq(article.id.to_s)
     end
 
     it "contains pagination links" do
-      _, article2, = create_list(:article, 3)
+      create_list(:article, 3)
       get "/articles", params: { page: {number: 2, size: 1} }
-      expect(json["links"].length).to eq(5)
-      expect(json["links"].keys).to contain_exactly(
-                                      "first", "prev", "next", "self", "last"
+      expect(json[:links].length).to eq(5)
+      expect(json[:links].keys).to contain_exactly(
+                                     :first, :prev, :next, :self, :last
                                     )
     end
   end
