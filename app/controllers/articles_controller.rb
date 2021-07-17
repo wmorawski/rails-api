7cde835a75ca762de55f0fde5433d47951cc4346
Wiktor Morawski <wiktor.morawski@recruitee.com>
2021-07-17 22:39:12 +0200

    feat: articles#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    render json: Article.find(params[:id])
+    render json: serializer.new(Article.find(params[:id]))
   end
 
   def serializer
