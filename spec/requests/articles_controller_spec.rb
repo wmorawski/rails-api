5fc427cf9163cb7ce3be405c696c73226aae9263
Wiktor Morawski <wiktor.morawski@recruitee.com>
2021-07-17 12:51:02 +0200

    test: article test improvement

---
+++ spec/requests/articles_controller_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe ArticlesController do
+  describe "#index" do
+    it "returns a success response" do
+      get "/articles"
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "returns a proper json" do
+      article = create :article
+      get "/articles"
+      body = JSON.parse(response.body).deep_symbolize_keys
+
+      expect(body).to eq(
+                        data: [{
+                                 id: article.id.to_s,
+                                 type: "article",
+                                 attributes: {
+                                   title: article.title,
+                                   content: article.content,
+                                   slug: article.slug
+                                 }
+                               }]
+                      )
+    end
+  end
+end
