84cacb8c2b096aea1c8f75edb232d1606df6d5dc
Wiktor Morawski <wiktor.morawski@recruitee.com>
2021-07-17 22:33:58 +0200

    test: articles#show

--- spec/requests/articles_controller_spec.rb
+++ spec/requests/articles_controller_spec.rb
@@ -34,18 +34,42 @@ RSpec.describe ArticlesController do
 
     it "paginate results" do
       _, article, = create_list(:article, 3)
-      get "/articles", params: { page: {number: 2, size: 1} }
+      get "/articles", params: { page: { number: 2, size: 1 } }
       expect(json_data.length).to eq(1)
       expect(json_data.first[:id]).to eq(article.id.to_s)
     end
 
     it "contains pagination links" do
       create_list(:article, 3)
-      get "/articles", params: { page: {number: 2, size: 1} }
+      get "/articles", params: { page: { number: 2, size: 1 } }
       expect(json[:links].length).to eq(5)
       expect(json[:links].keys).to contain_exactly(
                                      :first, :prev, :next, :self, :last
-                                    )
+                                   )
+    end
+
+    describe "#show" do
+      subject(:get_article) { get "/articles/#{article.id}" }
+
+      let(:article) { create :article }
+
+      before { :get_article }
+
+      it "returns a success response" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "returns a proper JSON" do
+        aggregate_failures do
+          expect(json_data[:id]).to eq(article.id.to_s)
+          expect(json_data[:type]).to eq("article")
+          expect(json_data[:attributes]).to eq(
+                                              title: article.title,
+                                              content: article.content,
+                                              slug: article.slug,
+                                            )
+        end
+      end
     end
   end
 end
